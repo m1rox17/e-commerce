2d97ecf423548a84c8edaa6b6885f00020c4affb
m1rox17 <yaroslavkostenko34@gmail.com>
2024-12-19 21:18:13 +0200

    Not Found design

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./error.scss";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="error__div">
+      <h1 className="error__title">404 Not Found</h1>
+      <p className="error__subtitle">
+        Your visited page not found. You may go home page.
+      </p>
+    </div>
+  );
 }
