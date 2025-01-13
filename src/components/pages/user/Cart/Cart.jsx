5503a247a0a069f3512d1da6d09e8e69b06b6d72
m1rox17 <yaroslavkostenko34@gmail.com>
2025-01-13 21:03:09 +0200

    fix footer

--- src/components/pages/user/Cart/Cart.jsx
+++ src/components/pages/user/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./Cart.scss";
+import "./cart.scss";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
--- src/components/template/Layout/Layout.jsx
+++ src/components/template/Layout/Layout.jsx
@@ -4,9 +4,11 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
+import "./layout.scss";
+
 export default function Layout({ role }) {
   return (
-    <div>
+    <div className="layout">
       <Header role={role} />
       <Outlet />
       <Footer />
