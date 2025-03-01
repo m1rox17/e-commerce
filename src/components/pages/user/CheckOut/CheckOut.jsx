17a81748325a99737071b66fb178c560ecb0bced
m1rox17 <yaroslavkostenko34@gmail.com>
2025-03-01 12:07:09 +0200

    Obligatory fields and telephone number

--- src/components/pages/user/CheckOut/CheckOut.jsx
+++ src/components/pages/user/CheckOut/CheckOut.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./check.scss";
+
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 import { useDispatch, useSelector } from "react-redux";
 import { auth } from "../../../../../firebase-config";
@@ -10,6 +13,8 @@ export default function CheckOut() {
   const { items } = useSelector((state) => state.cart);
   const total = useSelector(toatalItemsPrice);
 
+  const [value, setValue] = useState();
+
   useEffect(() => {
     if (auth.currentUser) {
       dispatch(fetchCart());
@@ -18,39 +23,43 @@ export default function CheckOut() {
     }
   }, [dispatch]);
 
+  const placeOrder = () => {
+    console.log(value);
+  };
+
   return (
     <div className="container">
       <div className="checkout">
         <div className="checkout__billing">
           <h2>Billing Details</h2>
-          <form className="billing__form">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              placeOrder();
+            }}
+            className="billing__form"
+            id="billing__form"
+          >
             <div className="form__group">
               <label>First Name*</label>
-              <input type="text" />
+              <input type="text" required />
             </div>
             <div className="form__group">
-              <label>Company Name</label>
-              <input type="text" />
+              <label>Shipping Address*</label>
+              <input type="text" required />
             </div>
             <div className="form__group">
-              <label>Street Address*</label>
-              <input type="text" />
-            </div>
-            <div className="form__group">
-              <label>Apartment, floor, etc. (optional)</label>
-              <input type="text" />
-            </div>
-            <div className="form__group">
-              <label>Town/City*</label>
-              <input type="text" />
+              <label for="email">Email Address*</label>
+              <input type="email" name="email" id="email" required />
             </div>
             <div className="form__group">
               <label>Phone Number*</label>
-              <input type="text" />
-            </div>
-            <div className="form__group">
-              <label>Email Address*</label>
-              <input type="email" />
+              <PhoneInput
+                defaultCountry="UA"
+                value={value}
+                onChange={setValue}
+                required
+              />
             </div>
             <div className="form__group checkbox__group">
               <input type="checkbox" id="saveInfo" />
@@ -83,11 +92,11 @@ export default function CheckOut() {
           </div>
           <div className="payment__options">
             <p>
-              <input type="radio" name="payment" id="bank" />{" "}
+              <input type="radio" name="payment" id="bank" />
               <label htmlFor="bank">Bank</label>
             </p>
             <p>
-              <input type="radio" name="payment" id="cash" defaultChecked />{" "}
+              <input type="radio" name="payment" id="cash" defaultChecked />
               <label htmlFor="cash">Cash on delivery</label>
             </p>
           </div>
@@ -95,7 +104,9 @@ export default function CheckOut() {
             <input type="text" placeholder="Coupon Code" />
             <button className="apply__coupon">Apply Coupon</button>
           </div>
-          <button className="place__order">Place Order</button>
+          <button type="submit" className="place__order" form="billing__form">
+            Place Order
+          </button>
         </div>
       </div>
     </div>
